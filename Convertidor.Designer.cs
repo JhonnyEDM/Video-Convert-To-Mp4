000a588a8dfe8c27572f63a75b8ae9b4c0813b50
Jhonny Ortiz <Josmeledm@gmail.com>
2019-04-03 08:32:53 +0800

    aplicación Funcional

---
+++ Convertidor.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Convertidor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Minimizar = new System.Windows.Forms.Button();
+            this.Cerrar = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(171, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Elegir Videos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(112, 412);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(257, 26);
+            this.progressBar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri Light", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Convertidor de video a MP4";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView);
+            this.panel1.Controls.Add(this.Minimizar);
+            this.panel1.Controls.Add(this.Cerrar);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.progressBar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 450);
+            this.panel1.TabIndex = 3;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre});
+            this.dataGridView.Location = new System.Drawing.Point(94, 159);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(292, 247);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // Minimizar
+            // 
+            this.Minimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Minimizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimizar.Location = new System.Drawing.Point(405, 3);
+            this.Minimizar.Name = "Minimizar";
+            this.Minimizar.Size = new System.Drawing.Size(35, 35);
+            this.Minimizar.TabIndex = 4;
+            this.Minimizar.Text = "-";
+            this.Minimizar.UseVisualStyleBackColor = true;
+            this.Minimizar.Click += new System.EventHandler(this.Minimizar_Click);
+            // 
+            // Cerrar
+            // 
+            this.Cerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cerrar.Location = new System.Drawing.Point(442, 3);
+            this.Cerrar.Name = "Cerrar";
+            this.Cerrar.Size = new System.Drawing.Size(35, 35);
+            this.Cerrar.TabIndex = 3;
+            this.Cerrar.Text = "X";
+            this.Cerrar.UseVisualStyleBackColor = true;
+            this.Cerrar.Click += new System.EventHandler(this.Cerrar_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 250;
+            // 
+            // Convertidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Convertidor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Cerrar;
+        private System.Windows.Forms.Button Minimizar;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+    }
+}
+
---
+++ Convertidor.cs
@@ -0,0 +1,103 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Convertidor : Form
+    {
+        string video;
+
+        public Convertidor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Proceso();
+        }
+
+        async void Proceso()
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                openFileDialog.Multiselect = true;
+
+                string formats = "*.webm; *.dat; *.wmv; *.3g2; *.3gp; *.3gp2; *.3gpp; *.amv; *.asf;  *.avi; *.bin; *.cue; *.divx; *.dv; *.flv; *.gxf; *.iso; *.m1v; *.m2v; *.m2t; *.m2ts; *.m4v; " +
+                          " *.mkv; *.mov; *.mp2; *.mp2v; *.mp4; *.mp4v; *.mpa; *.mpe; *.mpeg; *.mpeg1; *.mpeg2; *.mpeg4; *.mpg; *.mpv2; *.mts; *.nsv; *.nuv; *.ogg; *.ogm; *.ogv; *.ogx; *.ps; *.rec; *.rm; *.rmvb; *.tod; *.ts; *.tts; *.vob; *.vro";
+
+                string[] exts = formats.Split(';');
+                string filter = string.Empty;
+                foreach (string ext in exts)
+                {
+
+                    filter += "Video Files (" + ext.Replace("*", "").Trim() + ")|" + ext + "|";
+                }
+
+                openFileDialog.Filter = filter.Remove(filter.Length - 1, 1);
+                openFileDialog.ShowDialog();
+
+                foreach (var item in openFileDialog.SafeFileNames)
+                {
+                    dataGridView.Rows.Add(item);
+                }
+                   
+                foreach (var item in openFileDialog.FileNames)
+                {
+                    ActivarBarra();
+                    video = item;
+                    await Task.Run(() => ConvertirVideo(video));
+                    DesactivarBarra();
+                    dataGridView.Rows.Remove(dataGridView.Rows[0]);
+                }
+                MessageBox.Show("Todos Los Videos Se Han Convertido");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ha Ocurrido un error convirtiendo el video: " + ex.Message);
+            }
+        }
+
+        void ConvertirVideo(string video)
+        {
+            try
+            {
+                string path2 = Application.StartupPath;
+                var Convertidor = new FFMpegConverter();
+                string nombre =  Guid.NewGuid().ToString().Substring(0,8);
+                Convertidor.ConvertMedia(video, nombre + ".mp4", "mp4");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Este Video no se puede convertir: " + video + ex.Message);
+            }
+        }
+
+        async void ActivarBarra()
+        {
+            progressBar.MarqueeAnimationSpeed = 30;
+            progressBar.Style = ProgressBarStyle.Marquee;
+        }
+
+        async void DesactivarBarra()
+        {
+            progressBar.Style = ProgressBarStyle.Continuous;
+            progressBar.MarqueeAnimationSpeed = 0;
+        }
+
+        private void Cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
